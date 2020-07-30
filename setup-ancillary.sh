b03d7e26ac3a44fb64b156c1f860e2bb298972a9
Manoj Karthick Selva Kumar <manojkarthick@ymail.com>
2020-07-30 13:25:49 -0700

    Add installation instructions for kubernetes plugins using Krew

--- setup-ancillary.sh
+++ setup-ancillary.sh
@@ -12,7 +12,23 @@ curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s ht
 chmod u+x ./kubectl
 mv ./kubectl ~/bin/kubectl
 
+# To install kubernetes CLI plugins, first install krew
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+  "$KREW" update
+)
+# Sourced from https://krew.sigs.k8s.io/docs/user-guide/setup/install/#bash
+# Install plugins ctx and ns
+kubectl krew install ns
+kubectl krew install ctx
+
+
 # Install Google cloud SDK and CLI
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-303.0.0-darwin-x86_64.tar.gz
 tar xvf google-cloud-sdk-303.0.0-darwin-x86_64.tar.gz
 mv google-cloud-sdk ~/bin/
+
