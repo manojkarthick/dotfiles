5212dabc0a3e5bb418f0e45dff0291a3207eb3a2
Manoj Karthick Selva Kumar <manojkarthick@ymail.com>
2020-07-30 00:54:22 -0700

    Add script to download FiraCode font

---
+++ fira.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+echo "Downloading firacode fonts from master on github" 
+
+set -x
+
+mkdir -pv FiraCode && cd FiraCode
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Bold.ttf
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Light.ttf
+wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Medium.ttf
+wget https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Regular.ttf
+wget https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Retina.ttf
+wget https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-SemiBold.ttf
