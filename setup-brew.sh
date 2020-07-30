ede053b8437bc48f99264dde93aba9e23ec6a84e
Manoj Karthick Selva Kumar <manojkarthick@ymail.com>
2020-07-30 01:35:34 -0700

    Add comments and commands for pyenv/goenv installation

--- setup-brew.sh
+++ setup-brew.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+# By default, homebrew installs onto /usr/local/bin and updates the directory permissions
+# This is unsafe and should be avoided.
+# See: https://web.archive.org/web/20190925074708/https://applehelpwriter.com/2018/03/21/how-homebrew-invites-users-to-get-pwned/
+# Instead, this script downloads the homebrew tarball directly and uses the Untar Anywhere installation method.
+# See: https://docs.brew.sh/Installation#untar-anywhere
 mkdir ~/Homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/Homebrew
+
+# Install tools that are available through homebrew
+
 brew install wget
+
+# Install junegunn/fzf; Also install helpers and keybindings
 brew install fzf
 $(brew --prefix)/opt/fzf/install
+
+
+brew install pyenv
+
+# Install goenv from the HEAD since the last homebrew release does not have golang versions 1.12+
+brew install --HEAD goenv
 
